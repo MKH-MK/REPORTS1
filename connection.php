4f021b87b754e4b23a8888549d42caad99fbb140
MKH-MK <not4mak@gmail.com>
2024-11-21 12:52:09 +0000

    A bit more

--- connection.php
+++ connection.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $dbname = "reports";
 
-$conn = new PDO("mysql:host = $servername; dbname = $dbname", $username. $password);
+$conn = new PDO("mysql:host=$servername; dbname=$dbname", $username, $password);
 $conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 echo("connected!!");
 ?>
--- install.php
+++ install.php
@@ -1,6 +1,17 @@
 <?php
 include_once("connection.php");
-$statement = $conn->prepare("DROP TABLE IF EXISTS tblusers;");
+$statement = $conn->prepare("DROP TABLE IF EXISTS tblusers;
+CREATE TABLE TblUsers 
+(UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+Gender VARCHAR(1) NOT NULL,
+Surname VARCHAR(20) NOT NULL,
+Forename VARCHAR(20) NOT NULL,
+Password VARCHAR(20) NOT NULL,
+House VARCHAR(20) NOT NULL,
+Year INT(2) NOT NULL,
+Role TINYINT(1))
+");
+
 $statement -> execute();
 $statement -> closeCursor();
 ?>
