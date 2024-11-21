1d514f9977af4e0e201cb15aa860f0d32bc4a344
MKH-MK <not4mak@gmail.com>
2024-11-21 12:30:59 +0000

    First, connection thingy

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$servernam = "localhost";
+$username = "root";
+$password = "";
+$dbname = "reports";
+
+$conn = new PDO("mysql:host = $servername; dbname = $dbname", $username. $password);
+$conn -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+echo("connected!!");
+?>
